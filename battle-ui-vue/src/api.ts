3a81623991622b72a281eb0b351eaa65caa10dc9
liamsain <liamkennedy89@outlook.com>
2023-01-05 22:33:44 +0000

    try wss

--- battle-ui-vue/src/api.ts
+++ battle-ui-vue/src/api.ts
@@ -6,4 +6,4 @@ export const WebSocketPort = '3000';
 // export const ApiUrl = `http://${ApiIp}:${ApiPort}`;
 // export const WebSocketUrl = `ws://${ApiIp}:${WebSocketPort}`
 export const ApiUrl = `https://139-162-204-88.ip.linodeusercontent.com`;
-export const WebSocketUrl = `ws://139-162-204-88.ip.linodeusercontent.com:${WebSocketPort}`
+export const WebSocketUrl = `wss://139-162-204-88.ip.linodeusercontent.com:${WebSocketPort}`
